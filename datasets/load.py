ab8c5abcb82fbc4f7f4a2561d7545d3b1cee4802
yangmicA1 <yangmic@uci.edu>
2024-05-29 19:22:42 -0700

    Update load.py

--- datasets/load.py
+++ datasets/load.py
@@ -8,270 +8,149 @@ from PIL import Image
 import torch
 from torch.utils import data
 
-# Root directory of the project
 ROOT_DIR = os.path.abspath("../")
-
-# Import v2c utils
-sys.path.append(ROOT_DIR)  # To find local version of the library
+sys.path.append(ROOT_DIR)
 import main.utils as utils
 
-# ----------------------------------------
-# Functions for IIT-V2C Database Integration
-# ----------------------------------------
+def load_data(data_path=os.path.join('datasets', 'IIT-V2C'),
+              data_file='train.txt'):
+    def frame_range(start_frame, end_frame):
+        return list(range(start_frame, end_frame + 1))
 
-def load_annotations(dataset_path=os.path.join('datasets', 'IIT-V2C'),
-                     annotation_file='train.txt'):
-    """Helper function to parse IIT-V2C dataset.
-    """
-    def get_frames_no(init_frame_no, end_frame_no):
-        frames = []
-        for i in range(init_frame_no, end_frame_no+1, 1):
-            frames.append(i)
-        return frames
-
-    # Read annotations
     annotations = {}
-    with open(os.path.join(dataset_path, annotation_file), 'r') as f:
+    with open(os.path.join(data_path, data_file), 'r') as f:
         i = 0
-        annotation = []
+        lines = []
         for line in f:
             line = line.strip()
             i += 1
-            annotation.append(line)
+            lines.append(line)
 
             if i % 4 == 0:
-                # Test cases
-                #print(annotation)
-                #assert annotation[-1] == ''
-                #assert len(annotation[1].split(' ')) == 2
-                # Collect Video Name, Annotated Sub-Video Clip id
-                video_fname, video_id = '_'.join(annotation[0].split('_')[:-1]), annotation[0].split('_')[-1]
+                video_name, clip_id = '_'.join(lines[0].split('_')[:-1]), lines[0].split('_')[-1]
+                start_frame, end_frame = int(lines[1].split(' ')[0]), int(lines[1].split(' ')[1])
+                frames = frame_range(start_frame, end_frame)
+                command = lines[2].strip().split(' ')
 
-                # Collect init frame no ~ end frame no 
-                # Populate frames and commands
-                init_frame_no, end_frame_no = int(annotation[1].split(' ')[0]), int(annotation[1].split(' ')[1])
-                frames = get_frames_no(init_frame_no, end_frame_no)
-                command = annotation[2].strip().split(' ')
-
-                if video_fname not in annotations:
-                    annotations[video_fname] = [[video_id, frames, command]]
+                if video_name not in annotations:
+                    annotations[video_name] = [[clip_id, frames, command]]
                 else:
-                    annotations[video_fname].append([video_id, frames, command])
+                    annotations[video_name].append([clip_id, frames, command])
 
-                annotation = []
+                lines = []
 
     return annotations
 
-def summary(annotations):
-    """Helper function for IIT-V2C dataset summary.
-    """
-    num_clips = 0
-    num_frames = 0
-    for video_path in annotations.keys():
-        annotations_by_video = annotations[video_path]
-        num_clips += len(annotations_by_video)
-        for annotation in annotations_by_video:
-            num_frames += len(annotation[1])
-    print('# videos in total:', len(annotations))
-    print('# sub-video clips in annotation file:', num_clips)
-    print('# frames in total:', num_frames)
+def data_summary(data):
+    total_clips = 0
+    total_frames = 0
+    for video in data.keys():
+        video_annotations = data[video]
+        total_clips += len(video_annotations)
+        for annotation in video_annotations:
+            total_frames += len(annotation[1])
+    print('# videos in total:', len(data))
+    print('# sub-video clips:', total_clips)
+    print('# frames in total:', total_frames)
 
-def clipsname_captions(annotations,
-                       padding_words=True):
-    """Get (clip_name, target) pairs from annotation.
-    """
-    # Parse all (inputs, captions) pair
-    clips_name, captions = [], []
-    for video_fname in annotations.keys():
-        annotations_by_clip = annotations[video_fname]
-        for annotation in annotations_by_clip:
-            # Clip name
-            clip_name = video_fname + '_' + annotation[0]
-
-            # Get command caption
-            if padding_words:
-                target = '<sos> '+' '.join(annotation[2])+' <eos>'
+def extract_clips_captions(data, add_padding=True):
+    clips, captions = [], []
+    for video in data.keys():
+        video_annotations = data[video]
+        for annotation in video_annotations:
+            clip_name = video + '_' + annotation[0]
+            if add_padding:
+                caption = '<sos> ' + ' '.join(annotation[2]) + ' <eos>'
             else:
-                target = ' '.join(annotation[2])
+                caption = ' '.join(annotation[2])
+            clips.append(clip_name)
+            captions.append(caption)
+    return clips, captions
 
-            captions.append(target)
-            clips_name.append(clip_name)
-
-    return clips_name, captions
-
-
-# ----------------------------------------
-# For Custom Feature Extraction
-# ----------------------------------------
-
-def avi2frames(dataset_path, 
-               in_folder='avi_video',
-               out_folder='images'):
-    """Convert avi videos from IIT-V2C dataset into images.
-    WARNING: SLOW + TIME CONSUMING process! Final images take LARGE DISK SPACE!
-    """
+def convert_videos_to_frames(data_path, in_folder='avi_video', out_folder='images'):
     import cv2
-    videos_path = glob.glob(os.path.join(dataset_path, in_folder, '*.avi'))
-    for video_path in videos_path:
-        video_fname = video_path.strip().split('/')[-1][:-4]
-        save_path = os.path.join(dataset_path, out_folder, video_fname)
+    video_paths = glob.glob(os.path.join(data_path, in_folder, '*.avi'))
+    for video_path in video_paths:
+        video_name = video_path.strip().split('/')[-1][:-4]
+        save_path = os.path.join(data_path, out_folder, video_name)
         print('Saving:', save_path)
         if not os.path.exists(save_path):
             os.makedirs(save_path)
 
-        # OpenCV video feed
         cap = cv2.VideoCapture(video_path)
         success, frame = cap.read()
-        count = 0   # Start frame index as 0, as stated by the author
-        cap.set(cv2.CAP_PROP_FPS, 15)   # Force fps into 15, as stated by the author
+        count = 0
+        cap.set(cv2.CAP_PROP_FPS, 15)
         fps = cap.get(cv2.CAP_PROP_FPS)
-        print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
+        print("FPS: {}".format(fps))
 
         while success:
-            cv2.imwrite(os.path.join(save_path, 'frame%d.png' % count), frame)  # Save into loseless *.png format
+            cv2.imwrite(os.path.join(save_path, 'frame%d.png' % count), frame)
             count += 1
             success, frame = cap.read()
     return True
 
-def imgspath_targets_v1(annotations, 
-                        max_frames=30,
-                        dataset_path=os.path.join('datasets', 'IIT-V2C'),
-                        folder='images',
-                        synthetic_frame_path=os.path.join('imagenet_frame.png'),
-                        padding_words=True):
-    """Get training/test image-command pairs.
-    Version v2 strategy: Same as original IIT-V2C strategy, have a number 
-    of max_frames images per sample. Cut images larger than max_frames, populate
-    sample if no. images is smaller than max_frames.
-    """
-    def get_frames_path(frames_no,
-                        video_fname,
-                        max_frames=30,
-                        dataset_path=os.path.join('datasets', 'IIT-V2C'),
-                        folder='images',
-                        synthetic_frame_path=os.path.join('imagenet_frame.png')):
-        """Helper func to parse image path from numbers.
-        """
-        # Cut additional images by getting min loop factor
-        num_frames = len(frames_no)
+def prepare_image_paths(data, max_frames=30, data_path=os.path.join('datasets', 'IIT-V2C'), folder='images', synthetic_frame=os.path.join('imagenet_frame.png'), add_padding=True):
+    def get_image_paths(frame_numbers, video_name, max_frames=30, data_path=os.path.join('datasets', 'IIT-V2C'), folder='images', synthetic_frame=os.path.join('imagenet_frame.png')):
+        num_frames = len(frame_numbers)
         loop_factor = min(num_frames, max_frames)
 
-        imgs_path = []
+        paths = []
         for i in range(loop_factor):
-            img_path = os.path.join(dataset_path, folder, video_fname, 'frame{}.png'.format(frames_no[i]))
-            if os.path.isfile(img_path):    # Check if frame exists
-                imgs_path.append(img_path)
+            img_path = os.path.join(data_path, folder, video_name, 'frame{}.png'.format(frame_numbers[i]))
+            if os.path.isfile(img_path):
+                paths.append(img_path)
 
-        # Add synthetically made imagenet frame
-        while len(imgs_path) < max_frames:
-            imgs_path.append(synthetic_frame_path)
+        while len(paths) < max_frames:
+            paths.append(synthetic_frame)
 
-        return imgs_path
+        return paths
 
-    # Parse all (inputs, targets) pair
     inputs, targets = [], []
-    for video_fname in annotations.keys():
-        annotations_by_clip = annotations[video_fname]
-        for annotation in annotations_by_clip:
-            # Clip name
-            clip_name = video_fname + '_' + annotation[0]
-
-            # Get all images of the current clip
-            frames_path = get_frames_path(annotation[1], 
-                                          video_fname,
-                                          max_frames,
-                                          dataset_path,
-                                          folder,
-                                          synthetic_frame_path)
-
-            # Get command caption
-            if padding_words:
-                target = '<sos> '+' '.join(annotation[2])+' <eos>'
+    for video in data.keys():
+        video_annotations = data[video]
+        for annotation in video_annotations:
+            clip_name = video + '_' + annotation[0]
+            frames = get_image_paths(annotation[1], video, max_frames, data_path, folder, synthetic_frame)
+            if add_padding:
+                target = '<sos> ' + ' '.join(annotation[2]) + ' <eos>'
             else:
                 target = ' '.join(annotation[2])
-            
-            inputs.append({clip_name: frames_path})
+            inputs.append({clip_name: frames})
             targets.append(target)
 
     return inputs, targets
 
-# ----------------------------------------
-# Functions for torch.data.Dataset
-# ----------------------------------------
+def process_dataset(config, data_file, vocab=None, use_numpy=True):
+    annotations = load_data(config.DATASET_PATH, data_file)
 
-def parse_dataset(config, 
-                  annotation_file,
-                  vocab=None,
-                  numpy_features=True):
-    """Parse IIT-V2C dataset and update configuration.
-    """
-
-    # Load annotation 1st
-    annotations = load_annotations(config.DATASET_PATH, annotation_file)
-
-    # Pre-extracted features saved as numpy
-    if numpy_features:
-        clips, captions = clipsname_captions(annotations)
+    if use_numpy:
+        clips, captions = extract_clips_captions(annotations)
         clips = [os.path.join(config.DATASET_PATH, list(config.BACKBONE.keys())[0], x + '.npy') for x in clips]
-
-    # Use images
     else:
-        clips, captions = imgspath_targets_v1(annotations, 
-                                              max_frames=config.WINDOW_SIZE,
-                                              dataset_path=config.DATASET_PATH,
-                                              folder='images',
-                                              synthetic_frame_path=os.path.join(config.ROOT_DIR, 'datasets', 'imagenet_frame.png')
-                                             )
+        clips, captions = prepare_image_paths(annotations, max_frames=config.WINDOW_SIZE, data_path=config.DATASET_PATH, folder='images', synthetic_frame=os.path.join(config.ROOT_DIR, 'datasets', 'imagenet_frame.png'))
 
-    # Build vocabulary
     if vocab is None:
-        vocab = utils.build_vocab(captions, 
-                                  frequency=config.FREQUENCY,
-                                  start_word=config.START_WORD,
-                                  end_word=config.END_WORD,
-                                  unk_word=config.UNK_WORD)
-    # Reset vocab_size
+        vocab = utils.build_vocab(captions, frequency=config.FREQUENCY, start_word=config.START_WORD, end_word=config.END_WORD, unk_word=config.UNK_WORD)
     config.VOCAB_SIZE = len(vocab)
 
     if config.MAXLEN is None:
-        config.MAXLEN = utils.get_maxlen(captions) 
-            
-    # Process text tokens
+        config.MAXLEN = utils.get_maxlen(captions)
+
     targets = utils.texts_to_sequences(captions, vocab)
     targets = utils.pad_sequences(targets, config.MAXLEN, padding='post')
     targets = targets.astype(np.int64)
 
     return clips, targets, vocab, config
 
-class FeatureDataset(data.Dataset):
-    """Create an instance of IIT-V2C dataset with (features, targets) pre-extracted,
-    or with (imgs_path, targets)
-    """
-    def __init__(self, 
-                 inputs,
-                 targets,
-                 numpy_features=True,
-                 transform=None):
-        self.inputs, self.targets = inputs, targets     # Load annotations
-        self.numpy_features = numpy_features
+class CustomDataset(data.Dataset):
+    def __init__(self, inputs, targets, use_numpy=True, transform=None):
+        self.inputs = inputs
+        self.targets = targets
+        self.use_numpy = use_numpy
         self.transform = transform
 
-    def parse_clip(self, 
-                   clip):
-        """Helper function to parse images {clip_name: imgs_path} into a clip. 
-        """
-        Xv = []
-        clip_name = list(clip.keys())[0]
-        imgs_path = clip[clip_name]
-        for img_path in imgs_path:
-            img = self._imread(img_path)
-            Xv.append(img)
-        Xv = torch.stack(Xv, dim=0)
-        return Xv, clip_name
-
-    def _imread(self, path):
-        """Helper function to read image.
-        """
+    def _load_image(self, path):
         with open(path, 'rb') as f:
             img = Image.open(f)
             img = img.convert('RGB')
@@ -279,16 +158,24 @@ class FeatureDataset(data.Dataset):
             img = self.transform(img)
         return img
 
+    def _parse_clip(self, clip):
+        frames = []
+        clip_name = list(clip.keys())[0]
+        paths = clip[clip_name]
+        for path in paths:
+            img = self._load_image(path)
+            frames.append(img)
+        frames = torch.stack(frames, dim=0)
+        return frames, clip_name
+
     def __len__(self):
         return len(self.inputs)
 
     def __getitem__(self, idx):
-        # Pre-extracted numpy features
-        if self.numpy_features:
-            Xv = np.load(self.inputs[idx])
+        if self.use_numpy:
+            X = np.load(self.inputs[idx])
             clip_name = self.inputs[idx].split('/')[-1]
-        # Image dataset
         else:
-            Xv, clip_name = self.parse_clip(self.inputs[idx])
-        S = self.targets[idx]
-        return Xv, S, clip_name
+            X, clip_name = self._parse_clip(self.inputs[idx])
+        Y = self.targets[idx]
+        return X, Y, clip_name
